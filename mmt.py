bf6869c725032f6554e682756de113168c050b08
itsmanjeet <sainishaab550@gmail.com>
2019-07-31 13:49:57 +0530

    sanitize mmt

--- mmt.py
+++ mmt.py
@@ -2,19 +2,20 @@ from selenium import webdriver
 from time import sleep
 from bs4 import BeautifulSoup
 import sys
+import re
 from datetime import datetime
 data = {
     "place" : "Ganpatipule",
     "hotel" : 'Mango Valley Resort Ganpatipule',
     "checkin" : {
-        "date" : "28",
-        "month" : "07",
+        "date" : "01",
+        "month" : "08",
         "year" : "2019",
     },
 
     "checkout" : {
-        "date" : "31",
-        "month" : "07",
+        "date" : "02",
+        "month" : "08",
         "year" : "2019",
     },
 
@@ -54,29 +55,29 @@ class MMT:
         self.content = self.hotelsoup.find_all('div',attrs={'roomWrap'})
         now = datetime.now()
         self.Response = {
-            'position' : pos,
-            'ota' : 'mmt',
+            'listed_position' : pos,
+            'ota' : 'MMT',
             'runtime' : now.strftime("%y-%m-%d %H:%M:%S"),
-            'Data' : {
+            'check_in' : "%s/%s/%s" % (self.data['checkin']['date'],
+                                         self.data['checkin']['month'],
+                                         self.data['checkin']['year']),
 
-            },
+            'check_out' : "%s/%s/%s" % (self.data['checkout']['date'],
+                                          self.data['checkout']['month'],
+                                          self.data['checkout']['year'])
         }
         for row in self.content:
             left = row.find('div',attrs={'roomLeft'})
             RoomType = left.find('h2').text.strip()
-            FeatureSoup = left.find_all('li')
-            RoomFeatures = [f.text.strip() for f in FeatureSoup]
-            self.Response['Data'][RoomType] = {
-                'features' : RoomFeatures,
-            }
-            right = row.find_all('div',attrs={'roomRight'})
-            for r in right:
-                optsSoup = r.find_all('div',attrs={'bdrBottom'})
-                for j in optsSoup:
-                    a = [x.text.strip() for x in j.find_all('span')]
-                    options = a[1:]
-                    options.remove('')
-                    self.Response['Data'][RoomType][a[0]] = options
+            right = row.find_all('span',attrs={'bxNegotiate'})
+            r = [r.text.strip() for r in right]
+            pc = []
+            for p in r:
+                st = p.find('Total PayableINR')
+                price = (p[(st + len('Total PayableINR') + 1):])
+                pc.append(price)
+
+            self.Response[RoomType] = pc
 
         return self.Response
 
@@ -102,7 +103,6 @@ class Browser:
         self.opt.headless = True
         self.opt.add_argument('--ignore-certificate-errors')
         self.opt.add_argument('--test-type')
-        self.opt.binary = '/usr/bin/firefox'
     
     def Load(self,url):
         self.browser = webdriver.Firefox(executable_path="./driver/geckodriver", options=self.opt)
@@ -112,3 +112,4 @@ class Browser:
 
 m = MMT(data)
 response = m.GetResponse()
+print(response)
