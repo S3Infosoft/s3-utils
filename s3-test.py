273c7e12c29c130a01ca3d220804566eef364252
itsmanjeet <sainishaab550@gmail.com>
2019-08-04 19:31:35 +0530

    added test cases for booking.com

--- s3-test.py
+++ s3-test.py
@@ -1,16 +1,17 @@
 import unittest
 from mmt import MMT
+from booking import Bookingdotcom
 
-booking_data = {
+booking_input_valid = {
     'hotel' : 'Mango Valley Resort Ganpatipule',
     'search' : 'Ratnagiri',
     'checkin': {
-        'date' : '31',
-        'month': '07',
+        'date' : '07',
+        'month': '08',
         'year' : '2019',
     },
     'checkout': {
-        'date': '2',
+        'date': '08',
         'month': '08',
         'year': '2019',
     },
@@ -18,6 +19,42 @@ booking_data = {
     "rooms" : "1",
     "guests" : "2",
     "child" : "0",
+}
+
+
+booking_output_valid = {
+    'Standard Double Room': '₹2,700', 
+    'Superior Double Room': '₹3,600', 
+    'Deluxe Bungalow with Sea View': '₹7,500', 
+    'Three-Bedroom Bungalow': '₹7,500', 
+    'ota': 'Booking', 
+    'listed_position': '0', 
+    'check_in': '07/08/2019', 
+    'check_out': '08/08/2019'
+}
+
+
+booking_input_invalid = {
+    'hotel' : 'hotel not in list',
+    'search' : 'some wrong thing',
+    'checkin': {
+        'date' : '07',
+        'month': '08',
+        'year' : '2019',
+    },
+    'checkout': {
+        'date': '08',
+        'month': '08',
+        'year': '2019',
+    },
+
+    "rooms" : "1",
+    "guests" : "2",
+    "child" : "0",
+}
+
+booking_output_invalid = {
+    'status': 'FAIL'
 }
 
 mmt_input_valid = {
@@ -50,7 +87,7 @@ mmt_output_valid = {
     'Villa Oceanica Garden View': ['5,191', '5,710'], 
     'ota': 'MMT', 
     'Villa Oceanica Sea View': ['5,191', '6,229'], 
-    'listed_position': 2, 
+    'listed_position': 3, 
     'status': 'OK'
 }
 
@@ -77,17 +114,25 @@ mmt_output_invalid = {
 
 class resultTest(unittest.TestCase):
     def test_mmt_valid(self):
-        print("mmt test for valid input")
         m = MMT(mmt_input_valid)
         resp = m.get_response()
         resp.pop('runtime')
         self.assertEqual(resp,mmt_output_valid)
     
     def test_mmt_invalid(self):
-        print("mmt test for invalid input")
         m = MMT(mmt_input_invalid)
         resp = m.get_response()
         resp.pop("reason")
+        self.assertEqual(resp,mmt_output_invalid)
+
+    def test_booking_valid(self):
+        b = Bookingdotcom()
+        resp = b.get_response(booking_input_valid)
+        self.assertEqual(resp,mmt_output_valid)
+
+    def test_booking_invalid(self):
+        b = Bookingdotcom()
+        resp = b.get_response(booking_input_invalid)
         self.assertEqual(resp,mmt_output_invalid)
 
 if __name__ == '__main__':
